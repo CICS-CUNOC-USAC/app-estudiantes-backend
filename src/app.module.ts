f54433e169b11487143f95aa1ccf788840d4441c
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2023-07-25 10:37:22 -0600

    Initial implementation of migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseConfigurationModule } from './database/database-configuration.module';
 import { CourseModule } from './modules/course/course.module';
+import { CareerModule } from './modules/career/career.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseConfigurationModule,
     CourseModule,
+    CareerModule,
   ],
 })
 export class AppModule {}
