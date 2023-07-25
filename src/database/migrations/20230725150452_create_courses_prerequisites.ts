089c29324a7e1eee652e7cca95629a9944be6e86
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2023-07-25 14:35:28 -0600

    Created prerequisites migrations

--- src/database/migrations/20230725150452_create_courses_prerequisites.ts
+++ src/database/migrations/20230725150452_create_courses_prerequisites.ts
@@ -1,9 +1,21 @@
 import * as Knex from 'knex';
 
 export async function up(knex: Knex.Knex) {
-  return knex.schema;
+  return knex.schema.createTable('courses_prerequisites', (t) => {
+    t.increments('id').primary();
+    t.integer('course_code')
+      .notNullable()
+      .references('code')
+      .inTable('courses')
+      .onDelete('CASCADE');
+    t.integer('career_course_prerequisite_id')
+      .notNullable()
+      .references('id')
+      .inTable('career_courses_prerequisites')
+      .onDelete('CASCADE');
+  });
 }
 
 export async function down(knex: Knex.Knex) {
-  return knex.schema;
+  return knex.schema.dropTable('courses_prerequisites');
 }
--- src/database/migrations/20230725150516_create_credits_prerequisites.ts
+++ src/database/migrations/20230725150516_create_credits_prerequisites.ts
@@ -1,9 +1,17 @@
 import * as Knex from 'knex';
 
 export async function up(knex: Knex.Knex) {
-  return knex.schema;
+  return knex.schema.createTable('credits_prerequisites', (t) => {
+    t.increments('id').primary();
+    t.integer('career_course_prerequisite_id')
+      .notNullable()
+      .references('id')
+      .inTable('career_courses_prerequisites')
+      .onDelete('CASCADE');
+    t.integer('credits').notNullable();
+  });
 }
 
 export async function down(knex: Knex.Knex) {
-  return knex.schema;
+  return knex.schema.dropTable('credits_prerequisites');
 }
