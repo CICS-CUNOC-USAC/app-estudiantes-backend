d8644e885e48c4715930418e234ee6dcc4fb54df
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2023-09-04 16:46:19 -0600

    Updated course module

--- src/modules/course/entities/course.model.ts
+++ src/modules/course/entities/course.model.ts
@@ -1,4 +1,5 @@
-import { Model } from 'objection';
+import { Model, RelationMappings, RelationMappingsThunk } from 'objection';
+import { CareerModel } from 'src/modules/career/entities/career.model';
 
 export class CourseModel extends Model {
   static tableName = 'courses';
@@ -7,4 +8,21 @@ export class CourseModel extends Model {
   name: string;
   description: string;
   credits: number;
+
+  static get relationMappings(): RelationMappings | RelationMappingsThunk {
+    return {
+      careers: {
+        relation: Model.ManyToManyRelation,
+        modelClass: CareerModel,
+        join: {
+          from: 'courses.code',
+          through: {
+            from: 'career_courses.course_code',
+            to: 'career_courses.career_code',
+          },
+          to: 'careers.code',
+        },
+      },
+    };
+  }
 }
