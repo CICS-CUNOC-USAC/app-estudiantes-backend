1b0b03b9e6ce9fca2fdf2035051114c8fee529f4
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2023-09-04 16:44:12 -0600

    Added nest/swagger to proyect, added config in main file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NotFoundInterceptor } from './core/interceptors/not-found.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,23 @@ async function bootstrap() {
   // for a GET / UPDATE / DELETE endpoint, and returns Not Found error
   app.useGlobalInterceptors(new NotFoundInterceptor());
 
-  await app.listen(3000);
+  const docConfig = new DocumentBuilder()
+    .setTitle('Backend - Aplicación para estudiantes del CUNOC')
+    .setDescription(
+      'REST API para el proyecto de la nueva aplicación para estudiantes del CUNOC',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, docConfig);
+  // Order swagger collections alphabetically
+  const options = {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+    },
+  };
+  SwaggerModule.setup('apidoc', app, document, options);
+
+  await app.listen(8000);
 }
 bootstrap();
