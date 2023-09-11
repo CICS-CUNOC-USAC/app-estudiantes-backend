aac1c90c6a30d076235159e4d95d71d116c42df8
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2023-09-11 03:28:28 -0600

    Created staff auth strategies and guards

--- src/core/strategies/auth/staffs/jwt-staff.strategy.ts
+++ src/core/strategies/auth/staffs/jwt-staff.strategy.ts
@@ -1,28 +1,26 @@
-// import { PassportStrategy } from '@nestjs/passport';
-// import { Injectable } from '@nestjs/common';
-// import { ExtractJwt, Strategy } from 'passport-jwt';
-// import { StaffsService } from 'src/staffs/staffs.service';
-// import { EUserTypes } from 'src/database/models/user.model';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { StaffsService } from 'src/modules/staffs/staffs.service';
 
-// @Injectable()
-// export class JwtStaffStrategy extends PassportStrategy(Strategy, 'staff-jwt') {
-//   constructor(private readonly staffService: StaffsService) {
-//     super({
-//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//       ignoreExpiration: false,
-//       secretOrKey: process.env.JWTKEY,
-//     });
-//   }
+@Injectable()
+export class JwtStaffStrategy extends PassportStrategy(Strategy, 'staff-jwt') {
+  constructor(private readonly staffService: StaffsService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWTKEY,
+    });
+  }
 
-//   async validate(payload: object): Promise<any> {
-//     // return staff if present. If not, returns undefined
-//     if (!payload['profile_id']) {
-//       const staff = await this.staffService.findOne(payload['id']);
-//       if (!staff) {
-//         return null;
-//       }
-//       return { type: EUserTypes[EUserTypes.staff], ...staff };
-//     }
-//     return null;
-//   }
-// }
+  async validate(payload: object): Promise<any> {
+    // return staff if present. If not, returns undefined
+    if (!payload['profile_id']) {
+      const staff = await this.staffService.findAndReturnById(payload['id']);
+      if (!staff) {
+        return null;
+      }
+      return staff;
+    }
+  }
+}
--- src/core/strategies/auth/staffs/local-staff.strategy.ts
+++ src/core/strategies/auth/staffs/local-staff.strategy.ts
@@ -1,32 +1,30 @@
-// import { Strategy } from 'passport-local';
-// import { PassportStrategy } from '@nestjs/passport';
-// import { Injectable, UnauthorizedException } from '@nestjs/common';
-// import { IGeneralError } from 'src/core/interfaces/response/error/general-error.interface';
-// import { StaffAuthService } from 'src/auth/staffs/staff-auth.service';
-// import { StaffModel } from 'src/database/models/staff.model';
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { IGeneralError } from 'src/core/interfaces/response/error/general-error.interface';
+import { StaffAuthService } from 'src/modules/auth/staff/staff-auth.service';
+import { StaffModel } from 'src/modules/staffs/entities/staff.model';
 
-// @Injectable()
-// export class LocalStaffStrategy extends PassportStrategy(
-//   Strategy,
-//   'staff-login',
-// ) {
-//   constructor(private readonly authService: StaffAuthService) {
-//     super({ usernameField: 'email' });
-//   }
+@Injectable()
+export class LocalStaffStrategy extends PassportStrategy(
+  Strategy,
+  'staff-login',
+) {
+  constructor(private readonly staffAuthService: StaffAuthService) {
+    super({ usernameField: 'email' });
+  }
 
-//   async validate(email: string, password: string): Promise<any> {
-//     const staff: StaffModel = await this.authService.validateStaffCredentials(
-//       email,
-//       password,
-//     );
-//     if (!staff) {
-//       const error: IGeneralError = {
-//         statusCode: 401,
-//         message: 'Invalid staff credentials',
-//         error: 'Unauthorized',
-//       };
-//       throw new UnauthorizedException(error);
-//     }
-//     return staff;
-//   }
-// }
+  async validate(email: string, password: string): Promise<any> {
+    const staff: StaffModel =
+      await this.staffAuthService.validateStaffCredentials(email, password);
+    if (!staff) {
+      const error: IGeneralError = {
+        statusCode: 401,
+        message: 'Invalid staff credentials',
+        error: 'Unauthorized',
+      };
+      throw new UnauthorizedException(error);
+    }
+    return staff;
+  }
+}
