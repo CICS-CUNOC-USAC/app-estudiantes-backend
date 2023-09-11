9aa6c3a5be615bdae62bf169636e2747036efc6d
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2023-09-11 03:26:25 -0600

    Updated staffs module

--- src/modules/staffs/staffs.controller.ts
+++ src/modules/staffs/staffs.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { StaffsService } from './staffs.service';
 import { CreateStaffDto } from './dto/create-staff.dto';
 import { UpdateStaffDto } from './dto/update-staff.dto';
@@ -9,26 +17,26 @@ export class StaffsController {
 
   @Post()
   create(@Body() createStaffDto: CreateStaffDto) {
-    return this.staffsService.create(createStaffDto);
+    // return this.staffsService.create(createStaffDto);
   }
 
   @Get()
   findAll() {
-    return this.staffsService.findAll();
+    // return this.staffsService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.staffsService.findOne(+id);
+    // return this.staffsService.findOne(+id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateStaffDto: UpdateStaffDto) {
-    return this.staffsService.update(+id, updateStaffDto);
+    // return this.staffsService.update(+id, updateStaffDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.staffsService.remove(+id);
+    // return this.staffsService.remove(+id);
   }
 }
--- src/modules/staffs/staffs.service.ts
+++ src/modules/staffs/staffs.service.ts
@@ -1,26 +1,32 @@
-import { Injectable } from '@nestjs/common';
-import { CreateStaffDto } from './dto/create-staff.dto';
-import { UpdateStaffDto } from './dto/update-staff.dto';
+import { Inject, Injectable } from '@nestjs/common';
+import { ModelClass, Transaction } from 'objection';
+import { StaffModel } from './entities/staff.model';
 
 @Injectable()
 export class StaffsService {
-  create(createStaffDto: CreateStaffDto) {
-    return 'This action adds a new staff';
+  constructor(
+    @Inject(StaffModel.name) private staffModel: ModelClass<StaffModel>,
+  ) {}
+
+  /**
+   * Finds and returns an staff by email, or undefined if no staff is found
+   * @param {string} email Email to find a staff by
+   * @param {Transaction} trx? Optional transaction to use for the query
+   * @returns {Promise<StaffModel>} Staff found or undefined value
+   */
+  async findByEmail(email: string, trx?: Transaction): Promise<StaffModel> {
+    const staff = await this.staffModel.query(trx).findOne({ email });
+    return staff;
   }
 
-  findAll() {
-    return `This action returns all staffs`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} staff`;
-  }
-
-  update(id: number, updateStaffDto: UpdateStaffDto) {
-    return `This action updates a #${id} staff`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} staff`;
+  /**
+   * Finds and returns an staff by id, or undefined if no staff is found
+   * @param {string} id Id to find a staff by
+   * @param {Transaction} trx? Optional transaction to use for the query
+   * @returns {Promise<StaffModel>} Staff found or undefined value
+   */
+  async findAndReturnById(id: string, trx?: Transaction): Promise<StaffModel> {
+    const staff = await this.staffModel.query(trx).findById(id);
+    return staff;
   }
 }
