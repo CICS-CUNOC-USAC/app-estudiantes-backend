97f1cc4521f956ac9b15bb0c6a282def46a8a647
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2023-09-12 03:05:16 -0600

    Added relations attributes to user and profiles models

--- src/modules/users/entities/user.model.ts
+++ src/modules/users/entities/user.model.ts
@@ -4,11 +4,15 @@ import { ProfileModel } from 'src/modules/profiles/entities/profile.model';
 export class UserModel extends Model {
   static tableName = 'users';
 
+  // Attributes
   id: number;
   email: string;
   ra: string;
   encrypted_password: string;
   profile_id: number;
+
+  // Relations
+  profile: ProfileModel | null;
 
   static get relationMappings(): RelationMappings | RelationMappingsThunk {
     return {
