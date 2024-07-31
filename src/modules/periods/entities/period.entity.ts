fc4e7403769f5e93ed3b0057b19e7dbe559fc2c9
FernandoJRR <fercitotron@gmail.com>
2024-07-31 00:22:54 -0600

    feature: add relations to period model

--- src/modules/periods/entities/period.entity.ts
+++ src/modules/periods/entities/period.entity.ts
@@ -1,8 +1,22 @@
 import { Model } from 'objection';
+import { WeekdayModel } from 'src/modules/weekdays/entities/weekday.entity';
 
 export class PeriodModel extends Model {
   static tableName = 'periods';
 
   id: number;
   name: string;
+
+  static get relationMappings() {
+    return {
+      weekday: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: WeekdayModel,
+        join: {
+          from: 'periods.weekday_id',
+          to: 'weekdays.id',
+        },
+      },
+    };
+  }
 }
