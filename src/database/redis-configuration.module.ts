223fc05654213fe8fb1b5edc69636cb20819d7da
FernandoJRR <fercitotron@gmail.com>
2025-06-09 17:24:08 -0600

    fix: add error management to redis connection

--- src/database/redis-configuration.module.ts
+++ src/database/redis-configuration.module.ts
@@ -2,20 +2,28 @@ import { FactoryProvider } from "@nestjs/common";
 import Redis from "ioredis";
 
 export const redisClientFactory: FactoryProvider<Redis> = {
-    provide: 'RedisClient', 
-    useFactory: () => {
-        const redisinstance = new Redis({
-            host: process.env.REDIS_HOST,
-            port: parseInt(process.env.REDIS_PORT),
-            username: process.env.REDIS_USERNAME,
-            password: process.env.REDIS_PASSWORD,
-        });
+  provide: 'RedisClient',
+  useFactory: () => {
+    const redisinstance = new Redis({
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT),
+      username: process.env.REDIS_USERNAME,
+      password: process.env.REDIS_PASSWORD,
 
-        redisinstance.on('error', e =>{ 
-            throw new Error(`Error connecting to Redis: ${e}`)
-        });
+      retryStrategy: (times) => {
+        console.error(`Redis retry #${times}`);
+        if (times > 3) return null;
+        return 1000;
+      },
 
-        return redisinstance;
-    },
-    inject: []
+      reconnectOnError: () => false,
+    });
+
+    redisinstance.on('error', e => {
+      console.error(`Error connecting to Redis: ${e}`)
+    });
+
+    return redisinstance;
+  },
+  inject: []
 }
