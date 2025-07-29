0c2458f97324dacb58247bab4a8dd9b37b5255cd
FernandoJRR <fercitotron@gmail.com>
2025-07-29 11:57:03 -0600

    feat: add migrations for new sections

--- src/database/seeds/09-sections.seeder.ts
+++ src/database/seeds/09-sections.seeder.ts
@@ -1,7 +1,6 @@
 import * as Knex from 'knex';
 
 export async function seed(knex: Knex.Knex): Promise<any> {
-  await knex('sections').del();
   return knex('sections').insert([
     {
       name: 'A',
